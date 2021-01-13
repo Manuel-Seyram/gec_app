918556cf409283b8bc1deee81ced024093887e67
Manuel Seyram <mmamnuelson17@gmail.com>
2021-01-13 14:31:22 -0800

    Create bible.dart

---
+++ lib/Bible/bible.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+
+import 'package:gec_app/Bible/core.dart';
+import 'package:gec_app/Bible/idea/scope.dart';
+
+void main() => runApp(Bible());
+
+class Bible extends StatelessWidget {
+  const Bible({
+    Key key,
+    this.initialRoute,
+    this.isTesting: false,
+  }) : super(key: key);
+
+  final String initialRoute;
+  final bool isTesting;
+
+  @override
+  Widget build(BuildContext context) {
+    return ApplyModelBinding(
+      initialModel: ApplyThemeOption(
+        themeMode: ThemeMode.system,
+        textScaleFactor: systemTextScaleFactorOption,
+        customTextDirection: CustomTextDirection.localeBased,
+        locale: null,
+        timeDilation: timeDilation,
+        platform: defaultTargetPlatform,
+        isTesting: isTesting,
+      ),
+      child: Builder(
+          builder: (context) => MaterialApp(
+                title: Core.instance.appName,
+                debugShowCheckedModeBanner: false,
+                themeMode: ApplyThemeOption.of(context).themeMode,
+                theme: ApplyThemeData.lightThemeData.copyWith(
+                  platform: ApplyThemeOption.of(context).platform,
+                ),
+                darkTheme: ApplyThemeData.darkThemeData.copyWith(
+                  platform: ApplyThemeOption.of(context).platform,
+                ),
+                // localizationsDelegates: const [
+                //   ...GalleryLocalizations.localizationsDelegates,
+                //   LocaleNamesLocalizationsDelegate()
+                // ],
+                // supportedLocales: GalleryLocalizations.supportedLocales,
+                locale: ApplyThemeOption.of(context).locale,
+                localeResolutionCallback: (locale, supportedLocales) {
+                  deviceLocale = locale;
+                  // print(locale);
+                  return locale;
+                },
+                initialRoute: initialRoute,
+                onGenerateRoute: RouteConfiguration.onGenerateRoute,
+                // onUnknownRoute: RouteConfiguration.onUnknownRoute,
+              )),
+    );
+  }
+}
