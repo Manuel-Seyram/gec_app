54b334b4fcf4551949198855126f3beefa355afb
Manuel Seyram <mmamnuelson17@gmail.com>
2021-02-23 12:17:33 -0800

    Create resultpage.dart

---
+++ lib/Quiz/resultpage.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:gec_app/Quiz/home.dart';
+
+class resultpage extends StatefulWidget {
+  int marks;
+  resultpage({Key key, @required this.marks}) : super(key: key);
+  @override
+  _resultpageState createState() => _resultpageState(marks);
+}
+
+class _resultpageState extends State<resultpage> {
+  List<String> images = [
+    "assets/images/congratulations.jpg",
+    "assets/images/you can do better.jpg",
+    "assets/images/try again.jpg",
+  ];
+
+  String message;
+  String image;
+
+  @override
+  void initState() {
+    if (marks < 20) {
+      image = images[2];
+      message = "Read Your Bible and Try Again..\n" + "You Scored $marks";
+    } else if (marks < 35) {
+      image = images[1];
+      message = "You Can Do Better..\n" + "You Scored $marks";
+    } else {
+      image = images[0];
+      message = "Well Done!! You Study Your Bible...\n" + "You Scored $marks";
+    }
+    super.initState();
+  }
+
+  int marks;
+  _resultpageState(this.marks);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Result",
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 8,
+            child: Material(
+              elevation: 10.0,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      child: Container(
+                        width: 300.0,
+                        height: 300.0,
+                        child: ClipRect(
+                          child: Image(
+                            image: AssetImage(
+                              image,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 15.0,
+                        ),
+                        child: Center(
+                          child: Text(
+                            message,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontFamily: "Quando",
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OutlineButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => homepage(),
+                    ));
+                  },
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  borderSide: BorderSide(width: 3.0, color: Colors.blue),
+                  splashColor: Colors.blueAccent,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
