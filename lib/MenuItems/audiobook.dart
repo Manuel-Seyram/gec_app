04c783bd305671e4a832217c6f387354fecc0c74
Manuel Seyram <mmamnuelson17@gmail.com>
2021-04-14 05:32:45 -0700

    Create audiobook.dart

---
+++ lib/MenuItems/audiobook.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:gec_app/Menu.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+class Audiobook extends StatefulWidget {
+  @override
+  _AudiobookState createState() => new _AudiobookState();
+}
+
+class _AudiobookState extends State<Audiobook> {
+  InAppWebViewController webView;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('Gec Audioobook'),
+            leading: new IconButton(
+                icon: new Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                })),
+        body: Container(
+            child: Column(children: <Widget>[
+          Expanded(
+              child: InAppWebView(
+            initialUrl: "https://gecaudiobook.online",
+            initialHeaders: {},
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+                  debuggingEnabled: true,
+                  preferredContentMode: UserPreferredContentMode.DESKTOP),
+            ),
+            onWebViewCreated: (InAppWebViewController controller) {
+              webView = controller;
+            },
+            onLoadStart: (InAppWebViewController controller, String url) {},
+            onLoadStop:
+                (InAppWebViewController controller, String url) async {},
+          ))
+        ])),
+      ),
+    );
+  }
+}
