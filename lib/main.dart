514fae1bffe32dd11dfe5480d4cd7faf75a78d3a
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2021-08-25 11:31:47 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,13 @@
-import 'dart:async';
+//import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:gec_app/Menu.dart';
 
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: new SplashScreen(),
+    home: new HomeScreen(),
     routes: <String, WidgetBuilder>{
       '/Menu': (BuildContext context) => new HomeScreen()
     },
   ));
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  startTime() async {
-    var _duration = new Duration(seconds: 3);
-    return new Timer(_duration, navigationPage);
-  }
-
-  void navigationPage() {
-    Navigator.of(context).pushReplacementNamed('/Menu');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    startTime();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Image.asset('assets/images/globals.png'),
-      ),
-    );
-  }
 }
